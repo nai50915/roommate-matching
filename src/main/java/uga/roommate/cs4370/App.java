303a0a45959b9349ce80b83558eb62e13b7a7968
Shelby Osteen <sbo00779@uga.edu>
2025-04-30 11:26:12 -0400

    profile set Up

--- src/main/java/uga/roommate/cs4370/App.java
+++ src/main/java/uga/roommate/cs4370/App.java
@@ -1,1 +1,17 @@
+package uga.roommate.cs4370;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the application entry point. Do not change this unless you know 
+ * what you are doing.
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
