2d9acfaa28bcccd385cec759e3093775402535ad
netraA18 <netraamin13@gmail.com>
2025-04-30 19:59:26 -0400

    Updated ddl.sql

--- deliverables/ddl.sql
+++ deliverables/ddl.sql
@@ -1,1 +1,88 @@
+-- Create the database
+create database if not exists cs4370_roommate_platform;
 
+-- Use the database
+use cs4370_roommate_platform;
+
+-- Create the user table
+create table if not exists user ( 
+    userId int auto_increment,
+    username varchar(255) not null, 
+    password varchar(255) not null,
+    firstName varchar(255) not null,
+    lastName varchar(255) not null, 
+    description text,
+    imageUrl varchar(255),
+    primary key (userId), 
+    unique (username), 
+    constraint userName_min_length check (char_length(trim(userName)) >= 2),
+    constraint firstName_min_length check (char_length(trim(firstName)) >= 2), 
+    constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
+);
+
+-- Create the review table
+create table if not exists review (
+	reviewId int auto_increment,
+	reviewerId int not null,    /* who wrote the review */
+	revieweeId int not null,    /* who the review is about */
+	content text not null,
+	ratingValue int not null,
+	createdAt timestamp default current_timestamp,
+	primary key (reviewId),
+	foreign key (reviewerId) references user(userId) on delete cascade,
+	foreign key (revieweeId) references user(userId) on delete cascade
+);
+
+-- Create the vote table
+create table if not exists vote (
+	userId int not null,
+	reviewId int not null, 
+	type ENUM('upvote', 'downvote'),
+	primary key (userId, reviewId),
+	foreign key (reviewId) references review(reviewId) on delete cascade,
+    foreign key (userId) references user(userId) on delete cascade
+);
+
+-- Create the tag table
+create table if not exists tag (
+	tagId int auto_increment,
+	tagName varchar(255) not null, 
+	primary key(tagId)
+);
+
+-- Create the reviewTag table
+create table if not exists reviewTag (
+	tagId int, 
+	reviewId int not null, 
+	primary key (tagId, reviewId), 
+    foreign key (tagId) references tag(tagId) on delete cascade,
+    foreign key (reviewId) references review(reviewId) on delete cascade
+); 
+
+-- Create the userTag table
+create table if not exists userTag (
+	tagId int, 
+	userId int not null, 
+	primary key (tagId, userId), 
+    foreign key (tagId) references tag(tagId) on delete cascade,
+    foreign key (userId) references user(userId) on delete cascade
+);
+
+-- Create the match table
+create table if not exists `match` (
+	userA int not null, 
+	userB int not null, 
+	tags_match int,
+	primary key (userA, userB),
+	foreign key (userA) references user(userId) on delete cascade, 
+	foreign key (userB) references user(userId) on delete cascade
+);
+
+-- Create the pastRoommate table
+create table if not exists pastRoommate (
+	userA int not null,
+	userB int not null,
+	primary key (userA, userB),
+	foreign key (userA) references user(userId) on delete cascade, 
+	foreign key (userB) references user(userId) on delete cascade
+); 
