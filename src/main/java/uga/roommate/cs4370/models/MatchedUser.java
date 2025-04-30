e689173eaefd15706e56f4b5201412004061b1f2
nai50915 <nai50915@uga.edu>
2025-04-30 18:46:07 -0400

    Refining models

--- src/main/java/uga/roommate/cs4370/models/MatchedUser.java
+++ src/main/java/uga/roommate/cs4370/models/MatchedUser.java
@@ -5,14 +5,21 @@ package uga.roommate.cs4370.models;
  * indicating whether the current session user is matched with this user
  */
 public class MatchedUser extends User {
-    private final boolean isMatched;
+    private final boolean isMatched; // Whether user is a match of current session user
+    private final double percentageMatch;
 
-    public MatchedUser (String userId, String username, String firstName, String lastName, String bio, String imagePath, boolean isMatched) {
+    public MatchedUser (String userId, String username, String firstName, String lastName, String bio, String imagePath, boolean isMatched, double percentageMatch) {
         super(userId, username, firstName, lastName, bio, imagePath);
         this.isMatched = isMatched;
+        this.percentageMatch = percentageMatch;
     }
 
     public boolean isMatched() {
         return isMatched;
     }
+
+    public double getPercentageMatch() {
+        return percentageMatch;
+    }
+
 }
