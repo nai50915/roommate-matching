8ff7eda206b947d59a1ed80bb0667a518e3be145
Shelby Osteen <sbo00779@uga.edu>
2025-05-03 17:48:55 -0400

    front find past rm

--- src/main/java/uga/roommate/cs4370/controllers/FindPastRoommateController.java
+++ src/main/java/uga/roommate/cs4370/controllers/FindPastRoommateController.java
@@ -1,5 +1,36 @@
 package uga.roommate.cs4370.controllers;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import uga.roommate.cs4370.services.ReviewService;
+
+/**
+ * Handles /Matches URL
+ */
+@Controller
+@RequestMapping("/Find-Past-Roommate")
 public class FindPastRoommateController {
-    
+
+    /**
+     * Serves /Matches page
+     */
+    @GetMapping 
+    public ModelAndView webpage (@RequestParam(name = "error", required = false) String error) {
+        ModelAndView mv = new ModelAndView("find_past_roommates_page");
+        mv.addObject("errorMessage", error);
+        System.out.println("Going to find past roommates page...");
+        return mv;
+    }
+
 }
+
