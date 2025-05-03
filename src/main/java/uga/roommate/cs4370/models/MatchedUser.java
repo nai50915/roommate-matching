993c324d5c4a0f6e388d546a478844216c7cb54b
Nadia Ismail <151709494+nai50915@users.noreply.github.com>
2025-05-03 17:57:10 -0400

    Update MatchedUser.java

--- src/main/java/uga/roommate/cs4370/models/MatchedUser.java
+++ src/main/java/uga/roommate/cs4370/models/MatchedUser.java
@@ -1,5 +1,7 @@
 package uga.roommate.cs4370.models;
 
+import java.util.List;
+
 /**
  * Extends the User class to include a match status, 
  * indicating whether the current session user is matched with this user
@@ -8,11 +10,13 @@ public class MatchedUser extends User {
     private final boolean isMatched; // Whether user is a match of current session user
     private final double percentageMatch;
 
-    public MatchedUser (String userId, String username, String firstName, String lastName, String bio, String imagePath, boolean isMatched, double percentageMatch) {
+    public MatchedUser (String userId, String username, String firstName, String lastName, String bio, String imagePath, List<String> tags, boolean isMatched, double percentageMatch) {
         super(userId, username, firstName, lastName, bio, imagePath);
         this.isMatched = isMatched;
         this.percentageMatch = percentageMatch;
     }
+
+
 
     public boolean isMatched() {
         return isMatched;
