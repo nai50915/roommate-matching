bb3a73a76e1521d6dec9f3df56b0080f3a88bef4
Shelby Osteen <sbo00779@uga.edu>
2025-05-03 18:20:58 -0400

    past roomates front end

--- src/main/java/uga/roommate/cs4370/controllers/PastRoommateController.java
+++ src/main/java/uga/roommate/cs4370/controllers/PastRoommateController.java
@@ -1,5 +1,36 @@
 package uga.roommate.cs4370.controllers;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import uga.roommate.cs4370.services.ReviewService;
+
+/**
+ * Handles /Matches URL
+ */
+@Controller
+@RequestMapping("/Your-Past-Roommate")
 public class PastRoommateController {
-    
+
+    /**
+     * Serves /Your-Past-Roommate page
+     */
+    @GetMapping 
+    public ModelAndView webpage (@RequestParam(name = "error", required = false) String error) {
+        ModelAndView mv = new ModelAndView("past_roommates_page");
+        mv.addObject("errorMessage", error);
+        System.out.println("Going to past roommates page...");
+        return mv;
+    }
+
 }
+
