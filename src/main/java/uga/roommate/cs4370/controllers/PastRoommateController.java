4b1c18f34fde6b9939be6fcef65ca35231a4c199
netraA18 <netraamin13@gmail.com>
2025-05-03 16:41:36 -0400

    PastRoommateController.java

--- src/main/java/uga/roommate/cs4370/controllers/PastRoommateController.java
+++ src/main/java/uga/roommate/cs4370/controllers/PastRoommateController.java
@@ -1,51 +1,25 @@
 package uga.roommate.cs4370.controllers;
 
-
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import uga.roommate.cs4370.services.PastRoommateService;
-import uga.roommate.cs4370.models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-@RequestMapping("/pastroommate")
+@RequestMapping("/Your-Past-Roommate")
 public class PastRoommateController {
 
-   @Autowired
     private final PastRoommateService pastRoommateService;
 
     @Autowired
     public PastRoommateController(PastRoommateService pastRoommateService) {
         this.pastRoommateService = pastRoommateService;
-    }
-
-    // @GetMapping 
-    // public ModelAndView webpage (@RequestParam(name = "error", required = false) String error) {
-    //     ModelAndView mv = new ModelAndView("pastroommate");
-    //     mv.addObject("errorMessage", error);
-    //     System.out.println("Going to pastroommate page...");
-    //     return mv;
-    // }
-
-    @GetMapping
-    public ModelAndView showAllUsers(@RequestParam int userId) {
-        ModelAndView mv = new ModelAndView("findPastRoommates"); // findPastRoommates.html
-        try {
-            List<User> users = pastRoommateService.getAllUsersExcept(userId);
-            mv.addObject("users", users);
-        } catch (SQLException e) {
-            mv.addObject("error", "Could not load users.");
-        }
-        return mv;
     }
 
     @PostMapping("/add")
@@ -55,9 +29,16 @@ public class PastRoommateController {
         } catch (SQLException e) {
             return "redirect:/findPastRoommates?userId=" + currentUserId + "&error=Add failed";
         }
-        return "redirect:/pastRoommates?userId=" + currentUserId;
+        return "redirect:/Your-Past-Roommate?userId=" + currentUserId;
     }
 
-   
-    
+    @PostMapping("/remove")
+    public String removePastRoommate(@RequestParam int currentUserId, @RequestParam int targetUserId) {
+        try {
+            pastRoommateService.removePastRoommate(currentUserId, targetUserId);
+        } catch (SQLException e) {
+            return "redirect:/Your-Past-Roommate?userId=" + currentUserId + "&error=Remove failed";
+        }
+        return "redirect:/Your-Past-Roommate?userId=" + currentUserId;
+    }
 }
