9f0692e31fd0e4ba5333c97fb0e1d09b6a8961dc
Shelby Osteen <sbo00779@uga.edu>
2025-05-03 17:17:33 -0400

    load comments on profilez'

---
+++ src/main/java/uga/roommate/cs4370/models/ProfileReview.java
@@ -0,0 +1,39 @@
+package uga.roommate.cs4370.models;
+
+public class ProfileReview {
+    private final String reviewId;
+    private final String revieweeFirstName;
+    private final String reviewerFirstName;
+    private final String reviewerImg;
+    private final String content;
+
+    public ProfileReview (String reviewId, String revieweeFirstName, String reviewerFirstName, String reviewerImg, String content) {
+        this.reviewId = reviewId;
+        this.revieweeFirstName = revieweeFirstName;
+        this.reviewerFirstName = reviewerFirstName;
+        this.reviewerImg = reviewerImg;
+        this.content = content;
+    }
+
+    public String getReviewId() {
+        return reviewId;
+    }
+
+    public String getRevieweeFirstName() {
+        return revieweeFirstName;
+    }
+
+    public String getReviewerFirstName() {
+        return reviewerFirstName;
+    }
+
+    public String getReviewerImg() {
+        return reviewerImg;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+}
