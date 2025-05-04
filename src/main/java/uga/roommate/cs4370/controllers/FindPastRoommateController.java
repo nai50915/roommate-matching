d4ef0bdb2e9f172575bcf532636f83c17dade211
netraA18 <netraamin13@gmail.com>
2025-05-04 02:36:37 -0400

    Updated FindPastRoommateController

--- src/main/java/uga/roommate/cs4370/controllers/FindPastRoommateController.java
+++ src/main/java/uga/roommate/cs4370/controllers/FindPastRoommateController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,19 +36,34 @@ public class FindPastRoommateController {
             String userId =userService.getLoggedInUser().getUserId();
             System.out.println("User ID: " + userId);
             List<User> users = findPastRoommateService.getAllUsersExcept(userId);
-            System.out.println("Users: " + users.get(0).getFirstName());
-            System.out.println("Users: " + users.get(1).getFirstName());
-            System.out.println("Users: " + users.get(1).getImagePath());
+            System.out.println("Users: " + users.size());
+            // System.out.println("Users: " + users.get(0).getFirstName());
+            // System.out.println("Users: " + users.get(1).getFirstName());
+            // System.out.println("Users: " + users.get(1).getImagePath());
             
             if (error != null) {
 
                 mv.addObject("error", error);
             }
             mv.addObject("users", users);
+            mv.addObject("currentUserId", userId); 
         } catch (SQLException e) {
             mv.addObject("error", "Could not load users.");
         }
         return mv;
+    }
+
+    @PostMapping("/addPastRoommate")
+    public String addPastRoommate(@RequestParam("currentUserId") String currentUserId,
+                                  @RequestParam("newRoommateId") String newRoommateId) {
+                                    System.out.println("Adding past roommate:");;
+        try {
+            findPastRoommateService.addPastRoommate(Integer.parseInt(currentUserId), Integer.parseInt(newRoommateId));
+            return "redirect:/Your-Past-Roommate"; // Redirect back to the "Find Past Roommate" page after adding the roommate
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "redirect:/Your-Past-Roommate?error=Could%20not%20add%20past%20roommate"; // Redirect with error message
+        }
     }
 }
 
